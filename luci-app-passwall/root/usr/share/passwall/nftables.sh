204506fd43b10adfc1b7a38df0da59d03a04d125
lwb1978 <lwb1978@qq.com>
2024-06-27 20:33:05 +0800

    luci: 同步主分支到2024.06.27

--- luci-app-passwall/root/usr/share/passwall/nftables.sh
+++ luci-app-passwall/root/usr/share/passwall/nftables.sh
@@ -1236,6 +1236,7 @@ flush_nftset_reload() {
 	del_firewall_rule
 	flush_nftset
 	rm -rf /tmp/singbox_passwall*
+	rm -rf /tmp/etc/passwall_tmp/smartdns*
 	rm -rf /tmp/etc/passwall_tmp/dnsmasq*
 	/etc/init.d/passwall reload
 }
